652f9f65db41d51ad8c32df4c14c193eceb48bed
Ayana <ayanasamuel8@gmail.com>
2025-01-14 00:11:45 +0300

    edited the colors and added images

--- TodoList/script.js
+++ TodoList/script.js
@@ -31,19 +31,33 @@ function rander(array){
         newnote.classList.add('check');
         newnote.id=i;
 
+        let inputstorage=document.createElement('div');
+        inputstorage.id='inputstorage';
+
         let input=document.createElement('input');
+        input.id='check--box';
         input.type='checkbox';
         input.setAttribute('onclick','check(this)');
 
+        inputstorage.appendChild(input);
+
+        let imagestorage=document.createElement('div');
+        imagestorage.id='imgstorage';
+
+        let edit=document.createElement('img');
+        edit.id='pen';
+        edit.src='/pen.png';
+        edit.alt='edit';
+        edit.setAttribute('onclick','edit(this)');
+
         let image=document.createElement('img');
-        image.src='delete.png';
+        image.id='pen'
+        image.src='/delete.png';
         image.alt='delete'
         image.setAttribute('onclick','del(this.parentNode.id)');
 
-        let edit=document.createElement('img');
-        edit.src-'edit.png';
-        edit.alt='edit';
-        edit.setAttribute('onclick','edit(this)');
+        imagestorage.appendChild(edit);
+        imagestorage.appendChild(image);
 
         let note=document.createElement('div');
         note.classList.add('note');
@@ -52,11 +66,11 @@ function rander(array){
             input.checked=true;
             note.classList.add('checked');
         }
+
+        inputstorage.appendChild(note);
         if(array.includes(notes[i].status)){
-            newnote.appendChild(input);
-            newnote.appendChild(note);
-            newnote.appendChild(edit);
-            newnote.appendChild(image);
+            newnote.appendChild(inputstorage);
+            newnote.appendChild(imagestorage);
             ulElement.appendChild(newnote);
             ulElement.appendChild(document.createElement('hr'));
         }
@@ -92,15 +106,15 @@ document.getElementById('task').addEventListener('click',()=>{
     });
 //for checking the check-box
 function check(event){
-    console.log(notes[event.parentNode.id]);
-    let note=event.nextElementSibling;
+    console.log(event.parentNode.lastChild);
+    let note=event.parentNode.lastChild;
     note.classList.toggle('checked');
     if(event.checked){
-        notes[event.parentNode.id].status='completed';
+        notes[event.parentNode.parentNode.id].status='completed';
     }else{
-        notes[event.parentNode.id].status='pending';
+        notes[event.parentNode.parentNode.id].status='pending';
     }
-    console.log(notes[event.parentNode.id]);
+    console.log(notes[event.parentNode.parentNode.id]);
 }
 //to togle between dark and light
 function toggletoDark(){
@@ -114,12 +128,17 @@ function toggletoDark(){
         document.getElementById('addnote').style.color='white';
         document.getElementById('search').style.backgroundColor='black';
         document.getElementById('search').style.color='white';
+        document.getElementsByClassName('cancel')[0].style.backgroundColor='black';
+        // document.getElementById('check--box').style.accentColor='black';
+        console.log(document.getElementById('check--box'));
     }else{
         addNote.style.backgroundColor='aliceblue';
         document.getElementById('addnote').style.backgroundColor='aliceblue';
         document.getElementById('addnote').style.color='black';
         document.getElementById('search').style.backgroundColor='white';
         document.getElementById('search').style.color='darkgray';
+        document.getElementsByClassName('cancel')[0].style.backgroundColor='white';
+        document.getElementById('check--box').style.backgroundColor='white';
     }
 }
 //for note model pop up
@@ -138,11 +157,8 @@ function cancel(){
 function save(){
     let ulElement=document.querySelector('#list');
 
-    let newnote=document.createElement('div');
-    newnote.classList.add('check');
     let text=document.querySelector('#addnote').value;
     document.querySelector('#addnote').value='';
-    console.log(text);
     if(text!=''){
         let newnote=document.createElement('li');
         newnote.classList.add('check');
@@ -150,33 +166,45 @@ function save(){
 
         notes.push({content:text,status:'pending'})
 
+        let inputstorage=document.createElement('div');
+        inputstorage.id='inputstorage';
+
         let input=document.createElement('input');
+        input.id='check--box';
         input.type='checkbox';
         input.setAttribute('onclick','check(this)');
 
+        inputstorage.appendChild(input);
+
+        let imagestorage=document.createElement('div');
+        imagestorage.id='imgstorage';
+
         let image=document.createElement('img');
-        image.src='delete.png';
+        image.id='pen';
+        image.src='/delete.png';
         image.alt='delete'
         image.setAttribute('onclick','del(this.parentNode.id)');
 
         let edit=document.createElement('img');
-        edit.src-'edit.png';
+        edit.id='pen';
+        edit.src='/pen.png';
         edit.alt='edit';
         edit.setAttribute('onclick','edit(this)');
+
+        imagestorage.appendChild(edit);
+        imagestorage.appendChild(image);
 
         let note=document.createElement('div');
         note.classList.add('note');
         note.innerHTML=notes[notes.length-1].content;
 
-        newnote.appendChild(input);
-        newnote.appendChild(note);
-        newnote.appendChild(edit);
-        newnote.appendChild(image);
+        inputstorage.appendChild(note);
+        newnote.appendChild(inputstorage);
+        newnote.appendChild(imagestorage);
         ulElement.appendChild(document.createElement('hr'));
         ulElement.appendChild(newnote);
     }
     localStorage.setItem('notes',JSON.stringify(notes));
-    console.log(notes);
     let add=document.getElementById('add-note');
     add.style.display='none';
     document.getElementById('taskdiv').style.display='none';
@@ -203,10 +231,12 @@ function search(){
     let search=document.getElementById('search').value;
     search=search.toLowerCase();
     let notes=document.getElementsByClassName('note');
+    let found=false;
     for(let note of notes){
         console.log(note.parentNode);
         let str=note.innerHTML.toLowerCase();
         if(str.startsWith(search)){
+            found=true;
             note.parentNode.style.display='flex';
             note.parentNode.nextElementSibling.style.display='block';
         }else{
@@ -214,6 +244,11 @@ function search(){
             if(note.parentNode.previousElementSibling)
                 note.parentNode.previousElementSibling.style.display='none';
         }
+    }
+    if(!found){
+        document.getElementById('empty').style.display='flex';
+    }else{
+        document.getElementById('empty').style.display='none';
     }
     let ulElement=document.getElementById('list');
    ulElement.lastChild.style.display='none';
@@ -228,7 +263,9 @@ function del(index){
 }
 //for editing the note
 function edit(index){
-    let note=index.previousElementSibling;
+    let parent=index.parentNode;
+    let secondparent=parent.previousElementSibling;
+    let note=secondparent.lastChild;
     let text=note.innerHTML;
     let addnote=document.getElementById('addnote');
     addnote.value=text;
